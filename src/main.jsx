38ab6c81125e4a3a8b6da626c162107ffdbe4ce7
Sumini1 <sumini93.19@gmail.com>
2025-02-12 23:10:38 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "./context/ThemeContext";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
 import { Provider } from "react-redux";
 import store from "./store";
+
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    extensions: ['.js', '.jsx', '.json'],
     alias: {
-      '@': '/src'  // Ini akan memungkinkan Anda menggunakan @ sebagai alias untuk folder src
-    }
-  }
-})
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
